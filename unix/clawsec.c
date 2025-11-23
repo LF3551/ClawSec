1319bf30eb1c221b2cc1f6527561569f02de2f30
Aleksei Aleinikov <adk3551@gmail.com>
2025-11-23 19:19:25 +0200

    Update clawsec with latest code changes

--- unix/clawsec.c
+++ unix/clawsec.c
@@ -1,4 +1,12 @@
 #define _POSIX_C_SOURCE 200809L
+#include <sys/ioctl.h>
+#include <termios.h>
+
+#if defined(__linux__)
+#include <pty.h>
+#elif defined(__APPLE__)
+#include <util.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -295,13 +303,18 @@ static int relay_socket_stdio(int sockfd, int is_server, int chat_enabled) {
 
 #ifdef GAPING_SECURITY_HOLE
 static void run_encrypted_exec(int sockfd, const char *prog) {
-    int to_child[2];
-    int from_child[2];
+    int master_fd;
+    int slave_fd;
+    pid_t pid;
 
-    if (pipe(to_child) < 0 || pipe(from_child) < 0) fatal("pipe failed");
+    if (openpty(&master_fd, &slave_fd, NULL, NULL, NULL) < 0) {
+        fatal("openpty failed");
+    }
 
-    pid_t pid = fork();
-    if (pid < 0) fatal("fork failed");
+    pid = fork();
+    if (pid < 0) {
+        fatal("fork failed");
+    }
 
     if (pid == 0) {
         char *argv0;
@@ -309,33 +322,39 @@ static void run_encrypted_exec(int sockfd, const char *prog) {
         if (slash) argv0 = (char *)(slash + 1);
         else argv0 = (char *)prog;
 
-        close(to_child[1]);
-        close(from_child[0]);
+        close(master_fd);
 
-        dup2(to_child[0], STDIN_FILENO);
-        dup2(from_child[1], STDOUT_FILENO);
-        dup2(from_child[1], STDERR_FILENO);
+        if (setsid() < 0) {
+            _exit(127);
+        }
+#ifdef TIOCSCTTY
+        if (ioctl(slave_fd, TIOCSCTTY, NULL) < 0) {
+            _exit(127);
+        }
+#endif
 
-        close(to_child[0]);
-        close(from_child[1]);
+        dup2(slave_fd, STDIN_FILENO);
+        dup2(slave_fd, STDOUT_FILENO);
+        dup2(slave_fd, STDERR_FILENO);
+        if (slave_fd > STDERR_FILENO) {
+            close(slave_fd);
+        }
 
         execl(prog, argv0, (char *)NULL);
         _exit(127);
     }
 
-    close(to_child[0]);
-    close(from_child[1]);
+    close(slave_fd);
 
     char buf[BUFSIZE];
 
     for (;;) {
         fd_set rfds;
         int nfds = sockfd;
-
         FD_ZERO(&rfds);
         FD_SET(sockfd, &rfds);
-        FD_SET(from_child[0], &rfds);
-        if (from_child[0] > nfds) nfds = from_child[0];
+        FD_SET(master_fd, &rfds);
+        if (master_fd > nfds) nfds = master_fd;
 
         int ret = select(nfds + 1, &rfds, NULL, NULL, NULL);
         if (ret < 0) {
@@ -346,25 +365,25 @@ static void run_encrypted_exec(int sockfd, const char *prog) {
         if (FD_ISSET(sockfd, &rfds)) {
             ssize_t n = farm9crypt_read(sockfd, buf, sizeof(buf));
             if (n <= 0) break;
-            if (write_all(to_child[1], buf, (size_t)n) < 0) break;
+            if (write_all(master_fd, buf, (size_t)n) < 0) break;
         }
 
-        if (FD_ISSET(from_child[0], &rfds)) {
-            ssize_t n = read(from_child[0], buf, sizeof(buf));
+        if (FD_ISSET(master_fd, &rfds)) {
+            ssize_t n = read(master_fd, buf, sizeof(buf));
             if (n <= 0) break;
             ssize_t wn = farm9crypt_write(sockfd, buf, (size_t)n);
             if (wn != n) break;
         }
     }
 
-    close(to_child[1]);
-    close(from_child[0]);
+    close(master_fd);
     close(sockfd);
 
     int status;
     (void)waitpid(pid, &status, 0);
 }
 #endif
+
 
 static void usage(const char *prog) {
     fprintf(stderr,
